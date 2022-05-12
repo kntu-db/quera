b9f61f86a2bf3e84276e817fcfc6481fb2b85ac2
Mahdi Lotfi <mahdi.vahshat.80@gmail.com>
2022-05-13 03:07:14 +0430

    Initial Project

---
+++ web/src/main/java/ir/ac/kntu/web/WebApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/ir/ac/kntu/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
